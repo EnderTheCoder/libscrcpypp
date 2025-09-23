e4efc309c794f84b1f430331bbd81256e15fecd4
EnderTheCoder <ggameinvader@gmail.com>
2025-09-23 16:16:26 +0800

    update test

--- test/reconnect_test.cpp
+++ test/reconnect_test.cpp
@@ -8,15 +8,17 @@ int main(int argc, char *argv[]) {
     for (const auto &s: scrcpy::client::list_dev_serials("adb")) {
         std::cout << s << std::endl;
     }
-
     const auto cli = scrcpy::client::create_shared("localhost", 1234);
-    cli->deploy("adb", "scrcpy-server", "3.3.1", 1234);
 
-    while (true) {
+
+    for (int i = 0; i < 10; i++) {
         try {
+            cli->terminate();
+            cli->deploy("adb", "scrcpy-server", "3.3.1", 1234);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
             cli->connect();
             std::cout << "connected to server" << std::endl;
-            break;
+            // break;
         } catch (std::exception &e) {
             std::cerr << e.what() << std::endl;
         }
